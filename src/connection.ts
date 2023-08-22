311f684cde26c4e8dbd312b4b590d6af61f1c54c
alejandroquadri <ale@quadri.com.ar>
2023-08-22 12:48:16 -0300

    solucionado conexion a db

--- src/connection.ts
+++ src/connection.ts
@@ -5,7 +5,8 @@ class Mongo {
   // MONGODB_URI=mongodb://<username>:<password>@<cluster-address>/<database-name>
 
   public async connect() {
-    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lsaoaqz.mongodb.net/?retryWrites=true&w=majority`;
+    // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lsaoaqz.mongodb.net/?retryWrites=true&w=majority`;
+    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.c6fei51.mongodb.net/?retryWrites=true&w=majority`;
     this.client = await MongoClient.connect(uri);
     console.log('Connected to db');
     return this.client;
